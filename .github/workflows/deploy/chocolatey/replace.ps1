256624efc3049b561d894848fbee2f2deeb77f82
Ashish Padhy <100484401+Shurtu-gal@users.noreply.github.com>
2023-12-22 03:18:29 +0530

    temp

--- .github/workflows/deploy/chocolatey/replace.ps1
+++ .github/workflows/deploy/chocolatey/replace.ps1
@@ -11,5 +11,7 @@ $filePaths = @(
 foreach ($filePath in $filePaths) {
   $fileContents = Get-Content $filePath
   $fileContents = $fileContents -replace '{{version}}', $version
+  $fileContents = $fileContents -replace '{{checksum}}', $checksum
+  $fileContents = $fileContents -replace '{{checksum64}}', $checksum64
   Set-Content $filePath $fileContents
 }
